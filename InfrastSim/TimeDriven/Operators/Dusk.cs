a59e4ffa17823ef9787eddbb3e634bb27fdbfe24
Funny-ppt <1763341376@qq.com>
2023-10-23 15:12:23 +0800

    添加令、夕的心情检测阈值

--- InfrastSim/TimeDriven/Operators/Dusk.cs
+++ InfrastSim/TimeDriven/Operators/Dusk.cs
@@ -3,6 +3,8 @@ internal class Dusk : OperatorBase {
     public override string Name => "夕";
     static string[] _groups = { "岁" };
     public override string[] Groups => _groups;
+    static int[] _thresholds = new[] { 0, 12 };
+    public override int[] Thresholds => _thresholds;
 
     public override void Resolve(Simulator simu) {
         base.Resolve(simu);
--- InfrastSim/TimeDriven/Operators/Ling.cs
+++ InfrastSim/TimeDriven/Operators/Ling.cs
@@ -3,6 +3,8 @@ internal class Ling : OperatorBase {
     public override string Name => "令";
     static string[] _groups = { "岁" };
     public override string[] Groups => _groups;
+    static int[] _thresholds = new[] { 0, 12 };
+    public override int[] Thresholds => _thresholds;
 
     public override void Resolve(Simulator simu) {
         base.Resolve(simu);
