46ecd4a971a2fc68d7cc960e39f35e9b56a4ec0b
Funny-ppt <1763341376@qq.com>
2023-11-07 12:48:41 +0800

    同步代码修改

--- InfrastSimTest/SimulatorTest.cs
+++ InfrastSimTest/SimulatorTest.cs
@@ -123,11 +123,11 @@ namespace InfrastSimTest {
                     },
                     "B101": {
                         "type": "Trading",
-                        "level": 1
+                        "level": 3
                     },
                     "B102": {
                         "type": "Trading",
-                        "level": 1
+                        "level": 3
                     },
                     "B201": {
                         "type": "Manufacturing",
@@ -146,17 +146,20 @@ namespace InfrastSimTest {
                         "level": 3
                     },
                     "B303": {
-                        "type": "Manufacturing",
+                        "type": "Power",
                         "level": 3
                     },
                     "Dormitory 1": {
-                        "level": 1
+                        "level": 5
                     },
                     "Dormitory 2": {
-                        "level": 1
+                        "level": 5
                     },
                     "Dormitory 3": {
-                        "level": 1
+                        "level": 5
+                    },
+                    "Dormitory 4": {
+                        "level": 5
                     },
                     "Reception": {
                         "level": 3
@@ -230,13 +233,17 @@ namespace InfrastSimTest {
                         "type": "Power",
                         "level": 3
                     },
+                    "B303": {
+                        "type": "Power",
+                        "level": 3
+                    },
                     "B101": {
                         "type": "Trading",
-                        "level": 1
+                        "level": 3
                     },
                     "B102": {
                         "type": "Trading",
-                        "level": 1
+                        "level": 3
                     },
                     "B201": {
                         "type": "Manufacturing",
@@ -254,18 +261,17 @@ namespace InfrastSimTest {
                         "type": "Manufacturing",
                         "level": 3
                     },
-                    "B303": {
-                        "type": "Manufacturing",
-                        "level": 3
-                    },
                     "Dormitory 1": {
-                        "level": 1
+                        "level": 5
                     },
                     "Dormitory 2": {
-                        "level": 1
+                        "level": 5
                     },
                     "Dormitory 3": {
-                        "level": 1
+                        "level": 5
+                    },
+                    "Dormitory 4": {
+                        "level": 5
                     },
                     "Reception": {
                         "level": 3
@@ -281,101 +287,170 @@ namespace InfrastSimTest {
                     }
                 },
                 "ops": [
-                {
-                    "name": "森蚺",
-                    "positions": [
-                        "制造站",
-                        "控制中枢"
-                    ],
-                    "groups": [
-                        "依赖设施数量",
-                        "虚拟发电站"
-                    ],
-                    "relevant": [
-                        "依赖设施数量",
-                        "Lancet-2"
-                    ],
-                    "relevant_ops": [
-                        "Lancet-2"
-                    ]
-                },
-                {
-                    "name": "清流",
-                    "positions": [
-                        "制造站"
-                    ],
-                    "groups": [
-                        "依赖设施数量"
-                    ],
-                    "relevant": [
-                        "依赖设施数量"
-                    ]
-                },
-                {
-                    "name": "温蒂",
-                    "positions": [
-                        "制造站"
-                    ],
-                    "groups": [
-                        "依赖设施数量"
-                    ],
-                    "relevant": [
-                        "依赖设施数量"
-                    ]
-                },
-                {
-                    "name": "承曦格雷伊",
-                    "positions": [
-                        "发电站"
-                    ],
-                    "groups": [
-                        "作业平台",
-                        "异格",
-                        "虚拟发电站"
-                    ],
-                    "relevant": [
-                        "作业平台",
-                        "异格"
-                    ]
-                },
-                {
-                    "name": "Castle-3",
-                    "positions": [
-                        "制造站",
-                        "发电站"
-                    ],
-                    "prefer_product": [
-                        [
-                            "初级作战记录",
-                            0.3
-                        ]
-                    ],
-                    "groups": [
-                        "作业平台"
-                    ],
-                    "relevant": [
-                        "作业平台"
-                    ]
-                },
-                {
-                    "name": "Lancet-2",
-                    "positions": [
-                        "发电站",
-                        "宿舍"
-                    ],
-                    "groups": [
-                        "作业平台"
-                    ],
-                    "relevant": [
-                        "作业平台"
-                    ]
-                }
-            ]
+                    {
+                        "name": "\u6e05\u6d41",
+                        "fac": "\u5236\u9020\u7ad9",
+                        "positions": [
+                            "\u5236\u9020\u7ad9"
+                        ],
+                        "groups": [
+                            "\u4f9d\u8d56\u8bbe\u65bd\u6570\u91cf"
+                        ],
+                        "relevant": [
+                            "\u4f9d\u8d56\u8bbe\u65bd\u6570\u91cf"
+                        ],
+                        "product": "\u8d64\u91d1"
+                    },
+                    {
+                        "name": "\u68ee\u86ba",
+                        "fac": "\u5236\u9020\u7ad9",
+                        "positions": [
+                            "\u5236\u9020\u7ad9",
+                            "\u63a7\u5236\u4e2d\u67a2"
+                        ],
+                        "groups": [
+                            "\u4f9d\u8d56\u8bbe\u65bd\u6570\u91cf",
+                            "\u865a\u62df\u53d1\u7535\u7ad9"
+                        ],
+                        "relevant": [
+                            "\u4f9d\u8d56\u8bbe\u65bd\u6570\u91cf",
+                            "Lancet-2"
+                        ],
+                        "relevant_ops": [
+                            "Lancet-2"
+                        ],
+                        "product": "\u8d64\u91d1"
+                    },
+                    {
+                        "name": "\u68ee\u86ba",
+                        "fac": "\u63a7\u5236\u4e2d\u67a2",
+                        "positions": [
+                            "\u5236\u9020\u7ad9",
+                            "\u63a7\u5236\u4e2d\u67a2"
+                        ],
+                        "groups": [
+                            "\u4f9d\u8d56\u8bbe\u65bd\u6570\u91cf",
+                            "\u865a\u62df\u53d1\u7535\u7ad9"
+                        ],
+                        "relevant": [
+                            "\u4f9d\u8d56\u8bbe\u65bd\u6570\u91cf",
+                            "Lancet-2"
+                        ],
+                        "relevant_ops": [
+                            "Lancet-2"
+                        ],
+                        "product": "\u8d64\u91d1"
+                    },
+                    {
+                        "name": "\u6e29\u8482",
+                        "fac": "\u5236\u9020\u7ad9",
+                        "positions": [
+                            "\u5236\u9020\u7ad9"
+                        ],
+                        "groups": [
+                            "\u4f9d\u8d56\u8bbe\u65bd\u6570\u91cf"
+                        ],
+                        "relevant": [
+                            "\u4f9d\u8d56\u8bbe\u65bd\u6570\u91cf"
+                        ],
+                        "product": "\u8d64\u91d1"
+                    },
+                    {
+                        "name": "\u627f\u66e6\u683c\u96f7\u4f0a",
+                        "fac": "\u53d1\u7535\u7ad9",
+                        "positions": [
+                            "\u53d1\u7535\u7ad9"
+                        ],
+                        "groups": [
+                            "\u4f5c\u4e1a\u5e73\u53f0",
+                            "\u5f02\u683c",
+                            "\u865a\u62df\u53d1\u7535\u7ad9"
+                        ],
+                        "relevant": [
+                            "\u4f5c\u4e1a\u5e73\u53f0",
+                            "\u5f02\u683c"
+                        ],
+                        "product": "\u8d64\u91d1"
+                    },
+                    {
+                        "name": "Lancet-2",
+                        "fac": "\u53d1\u7535\u7ad9",
+                        "positions": [
+                            "\u53d1\u7535\u7ad9",
+                            "\u5bbf\u820d"
+                        ],
+                        "groups": [
+                            "\u4f5c\u4e1a\u5e73\u53f0"
+                        ],
+                        "relevant": [
+                            "\u4f5c\u4e1a\u5e73\u53f0"
+                        ],
+                        "product": "\u8d64\u91d1"
+                    },
+                    {
+                        "name": "Lancet-2",
+                        "fac": "\u5bbf\u820d",
+                        "positions": [
+                            "\u53d1\u7535\u7ad9",
+                            "\u5bbf\u820d"
+                        ],
+                        "groups": [
+                            "\u4f5c\u4e1a\u5e73\u53f0"
+                        ],
+                        "relevant": [
+                            "\u4f5c\u4e1a\u5e73\u53f0"
+                        ],
+                        "product": "\u8d64\u91d1"
+                    },
+                    {
+                        "name": "Castle-3",
+                        "fac": "\u5236\u9020\u7ad9",
+                        "positions": [
+                            "\u5236\u9020\u7ad9",
+                            "\u53d1\u7535\u7ad9"
+                        ],
+                        "prefer_product": [
+                            [
+                                "\u521d\u7ea7\u4f5c\u6218\u8bb0\u5f55",
+                                0.3
+                            ]
+                        ],
+                        "groups": [
+                            "\u4f5c\u4e1a\u5e73\u53f0"
+                        ],
+                        "relevant": [
+                            "\u4f5c\u4e1a\u5e73\u53f0"
+                        ],
+                        "product": "\u521d\u7ea7\u4f5c\u6218\u8bb0\u5f55"
+                    },
+                    {
+                        "name": "Castle-3",
+                        "fac": "\u53d1\u7535\u7ad9",
+                        "positions": [
+                            "\u5236\u9020\u7ad9",
+                            "\u53d1\u7535\u7ad9"
+                        ],
+                        "prefer_product": [
+                            [
+                                "\u521d\u7ea7\u4f5c\u6218\u8bb0\u5f55",
+                                0.3
+                            ]
+                        ],
+                        "groups": [
+                            "\u4f5c\u4e1a\u5e73\u53f0"
+                        ],
+                        "relevant": [
+                            "\u4f5c\u4e1a\u5e73\u53f0"
+                        ],
+                        "product": "\u8d64\u91d1"
+                    }
+                ],
+                "max_size": 100
             }
             """;
             using var ms = new MemoryStream();
             using var writer = new Utf8JsonWriter(ms);
-            EnumerateHelper.Enumerate(JsonDocument.Parse(input), writer);
+            EnumerateHelper.Enumerate(JsonDocument.Parse(input2), writer);
             Console.WriteLine(Encoding.UTF8.GetString(ms.GetBuffer(), 0, (int)ms.Length));
         }
     }
