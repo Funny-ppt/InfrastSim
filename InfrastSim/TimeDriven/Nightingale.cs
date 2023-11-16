dc28951e7ec7370b70489b831147130612c37f69
Funny-ppt <1763341376@qq.com>
2023-11-16 12:24:12 +0800

    添加褐果、夜莺、闪灵

---
+++ InfrastSim/TimeDriven/Nightingale.cs
@@ -0,0 +1,13 @@
+namespace InfrastSim.TimeDriven.Operators;
+internal class Nightingale : OperatorBase {
+    public override string Name => "夜莺";
+
+    public override void Resolve(Simulator simu) {
+        base.Resolve(simu);
+
+        if (Facility is Dormitory dorm) {
+            dorm.SetDormMoodModifier(Upgraded >= 2 ? -0.2 : -0.1);
+        }
+    }
+}
+
---
+++ InfrastSim/TimeDriven/Shining.cs
@@ -0,0 +1,13 @@
+namespace InfrastSim.TimeDriven.Operators;
+internal class Shining : OperatorBase {
+    public override string Name => "闪灵";
+
+    public override void Resolve(Simulator simu) {
+        base.Resolve(simu);
+
+        if (Facility is Dormitory dorm) {
+            dorm.SetVipMoodModifier(Upgraded >= 2 ? -0.75 : -0.55);
+        }
+    }
+}
+
