21c845ab01f84721fa72095075014814ed894e2e
funny-ppt <1763341376@qq.com>
2023-12-17 19:50:13 +0800

    尝试修复清流槐琥问题 #3

--- InfrastSim/TimeDriven/Priority.cs
+++ InfrastSim/TimeDriven/Priority.cs
@@ -2,8 +2,8 @@ namespace InfrastSim.TimeDriven;
 internal static class Priority {
     public const int PropConversion = 20;
     public const int Common = 100;
-    public const int AccordingToFacilityAmount = 110;
-    public const int WaaiFu = 120;
+    public const int WaaiFu = 110;
+    public const int AccordingToFacilityAmount = 120;
     public const int Degenbrecher = 160;
     public const int Jaye = 170;
     public const int Swire = 180;
